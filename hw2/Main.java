45d0fb69d2c954fda78f49fc1ccd09651502819c
Alex <kccnbnb@mail.ru>
2023-04-03 22:19:10 +0300

    done HW2

--- hw2/Main.java
+++ hw2/Main.java
@@ -1,5 +1,6 @@
 package hw2;
 
+import java.io.IOException;
 import java.lang.System.Logger;
 import java.lang.System.Logger.Level;
 import java.util.Arrays;
@@ -10,13 +11,21 @@ import hw2.task.Task1;
 
 public class Main {
     
-    private static final java.util.logging.Logger LOG = Log.log(Main.class.getName());
-    public static void main(String[] args) {
-        int [] array = {0, 4, 2, 5, 3, 17, 8};
-        LOG.info("Исходный массив: " + Arrays.toString(array));
-        Task1.sotredArray(array);
-        LOG.info("Отсортированный массив: " + Arrays.toString(array));
-        Task1.printArray(array);
+    // private static final java.util.logging.Logger LOG = Log.log(Main.class.getName());
+    // public static void main(String[] args) {
+    //     int [] array = {0, 4, 2, 5, 3, 17, 8};
+    //     LOG.info("Исходный массив: " + Arrays.toString(array));
+    //     Task1.sotredArray(array);
+    //     LOG.info("Отсортированный массив: " + Arrays.toString(array));
+    //     Task1.printArray(array);
+    // }
+
+    public static void main(String[] args) throws Exception {
+        String [] arrayData =  Task1.ReadLineFromFile("data.txt");
+        for(int i = 0; i < arrayData.length; i++) {
+            System.out.println(Task1.PrintLine(arrayData[i]));
+        }
+        
     }
     
 }
--- hw2/task/Task1.java
+++ hw2/task/Task1.java
@@ -1,12 +1,16 @@
 //Реализуйте алгоритм сортировки пузырьком числового массива, результат после каждой итерации запишите в лог-файл.
 
 package hw2.task;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+
 public class Task1 {
 
     public static void sotredArray(int[] arr) {
-        
+
         for (int i = 0; i < arr.length; i++) {
-            for (int j = 1; j < arr.length-1; j++) {
+            for (int j = 1; j < arr.length - 1; j++) {
                 if (arr[j] > arr[j + 1]) {
                     int max = arr[j];
                     arr[j] = arr[j + 1];
@@ -14,11 +18,11 @@ public class Task1 {
                 }
 
             }
-            
+
         }
 
     }
-        
+
     public static void printArray(int[] arr) {
         for (int k = 0; k < arr.length; k++) {
             System.out.println(arr[k]);
@@ -27,4 +31,57 @@ public class Task1 {
 
     }
 
+    // 2. Дана строка json:
+    // [{"фамилия":"Иванов","оценка":"5","предмет":"Математика"},
+    // {"фамилия":"Петрова","оценка":"4","предмет":"Информатика"},
+    // {"фамилия":"Краснов","оценка":"5","предмет":"Физика"}]
+    // Написать метод, который распарсит этот текст и, используя StringBuilder,
+    // создаст строки вида:
+    // Студент [фамилия] получил [оценка] по предмету [предмет]. Например:
+    // Студент Иванов получил 5 по предмету Математика.
+    // Студент Петрова получил 4 по предмету Информатика.
+    // Студент Краснов получил 5 по предмету Физика.
+    public static String[] ReadLineFromFile(String pathway) throws Exception {
+        BufferedReader br = new BufferedReader(new FileReader(pathway));
+        String str;
+        int size = 0;
+        while ((str = br.readLine()) != null) {
+            size += 1;
+        }
+        br.close();
+        String[] listData = new String[size];
+
+        int i = 0;
+        BufferedReader br1 = new BufferedReader(new FileReader(pathway));
+        while ((str = br1.readLine()) != null) {
+            listData[i] = str;
+            i += 1;
+        }
+        br1.close();
+        return listData;
+
+    }
+
+    public static StringBuilder PrintLine(String line) {
+        String line1 = line.replace("{", "");
+        String line2 = line1.replace("}", "");
+        String line3 = line2.replaceAll("\"", "");
+        String line4 = line3.replace("[", "");
+        String line5 = line4.replace("]", "");
+        StringBuilder result = new StringBuilder("");
+        String [] arrayData = line5.split(",");
+        String [] arrayName = {"Студент ", " получил ", " по предмету "};
+        for (int i =0; i < arrayData.length; i++) {
+            String[] arrData = arrayData[i].split(":");
+            result.append(arrayName[i]);
+            result.append(arrData[1]);
+            }
+        return result;
+
+
+
+
+
+
+}
 }
